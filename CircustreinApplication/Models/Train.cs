742955df4e3a83960059045cd7275644147e6121
Calin Peters <calinpeters98@hotmail.nl>
2020-10-28 15:15:58 +0100

    Added implementation for Train & Wagon classes;

---
+++ CircustreinApplication/Models/Train.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircustreinApplication.Models
+{
+    public class Train
+    {
+        private int _totalWagons;
+        public List<Wagon> Wagons { get; private set; }
+
+        public Train()
+        {
+            Wagons = new List<Wagon>();
+        }
+
+        public bool SortInWagons(List<Animal> animals)
+        {
+            bool isSorted = false;
+            foreach (var animal in animals)
+            {
+                var availableWagon = CheckIfWagonAvailable(animal);
+                if (availableWagon != null)
+                {
+                    availableWagon.Animals.Add(animal);
+                    isSorted = true;
+                }
+                else
+                {
+                    availableWagon = new Wagon();
+                    availableWagon.Animals.Add(animal);
+                }
+            }
+
+            return isSorted;
+        }
+
+        public Wagon CheckIfWagonAvailable(Animal animal)
+        {
+            Wagon available = null;
+
+            foreach (var wagon in Wagons)
+            {
+                for (int i = 0; i < wagon.Animals.Count; i++)
+                {
+                    if (wagon.AnimalIsCompatible(animal) && wagon.CheckWagonCapacity(animal))
+                    {
+                        available = wagon;
+                    }
+                    
+                    available = null;
+                    
+                }
+            }
+
+            return available;
+        }
+
+
+    }
+}
--- CircustreinApplication/Models/Wagon.cs
+++ CircustreinApplication/Models/Wagon.cs
@@ -12,7 +12,39 @@ namespace CircustreinApplication.Models
         public Wagon()
         {
             Capacity = 10;
+            Animals = new List<Animal>();
         }
+
+        public bool CheckWagonCapacity(Animal animal)
+        {
+            if (Capacity - animal.Size > 0)
+            {
+                Animals.Add(animal);
+                Capacity -= Convert.ToInt32(animal.Size);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool AnimalIsCompatible(Animal animal)
+        {
+            bool isCompatible = false;
+            for (int i = 0; i < Animals.Count; i++)
+            {
+                if (animal.Size == Animals[i].Size && animal.Diet == Animals[i].Diet && CheckWagonCapacity(animal))
+                {
+                    isCompatible = true;
+                }
+                else
+                {
+                    isCompatible = false;
+                }
+            }
+
+            return isCompatible;
+        }
+
 
 
 
