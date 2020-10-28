cbf711fc0272ab843a63f020083904e5a67bda2a
Calin Peters <calinpeters98@hotmail.nl>
2020-10-28 12:53:55 +0100

    Added AnimalCollection & Wagon class; added a way to sort lists based on animal size.

---
+++ CircustreinApplication/Models/Wagon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircustreinApplication.Models
+{
+    public class Wagon
+    {
+        public int Capacity { get; private set; }
+        public List<Animal> Animals { get; set; }
+
+        public Wagon()
+        {
+            Capacity = 10;
+        }
+
+
+
+
+    }
+}
