0ced210adc0a7d29c398c626c81e5915abf5bed6
Calin Peters <calinpeters98@hotmail.nl>
2020-10-28 11:11:46 +0100

    Initial commit

---
+++ CircustreinGUI/CircustreinGUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CircustreinGUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
