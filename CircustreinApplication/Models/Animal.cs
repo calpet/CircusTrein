f580d2a9c8ff9c327d3004ef73285d0fc5f3cbf1
Calin Peters <calinpeters98@hotmail.nl>
2020-10-29 15:06:36 +0100

    Added ToString() to Animal class in order to make it a rich model.

--- CircustreinApplication/Models/Animal.cs
+++ CircustreinApplication/Models/Animal.cs
@@ -6,5 +6,10 @@ namespace CircustreinApplication
     {
         public Size Size { get; set; }
         public Diet Diet { get; set; }
+
+        public override string ToString()
+        {
+            return "Size: " + Size + "\nDiet: " + Diet;
+        }
     }
 }
