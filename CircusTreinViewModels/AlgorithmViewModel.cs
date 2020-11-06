4b9dc8f54b4359bb6d3d03f52cc0ee69d1e0210b
Calin Peters <calinpeters98@hotmail.nl>
2020-11-06 14:15:17 +0100

    Initial commit Unit tests.

---
+++ CircusTreinViewModels/AlgorithmViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using CircustreinApplication;
+using CircustreinApplication.Models;
+
+namespace CircusTreinViewModels
+{
+    public class AlgorithmViewModel
+    {
+        private Animal _animal;
+        private AnimalCollection _collection;
+        private Wagon _wagon;
+        private Train _train;
+        private List<Animal> _animals;
+
+        public AlgorithmViewModel()
+        {
+            _animal = new Animal();
+            _collection = new AnimalCollection();
+            _wagon = new Wagon();
+            _train = new Train();
+            _animals = new List<Animal>();
+        }
+
+        public Train StartAlgorithms(List<Animal> animals)
+        {
+            var sortedList = _collection.SortBySize(animals);
+            _train.SortInWagons(sortedList);
+
+            return _train;
+        }
+
+        public List<Animal> GenerateRandomAnimals(int amount)
+        {
+            Random rDiet = new Random();
+            Random rSize = new Random();
+            Array dValues = Enum.GetValues(typeof(Diet));
+            Array sValues = Enum.GetValues(typeof(Size));
+
+            for (int i = 0; i < amount; i++)
+            {
+                Animal animal = new Animal() { Diet = (Diet)dValues.GetValue(rDiet.Next(dValues.Length)), Size = (Size)sValues.GetValue(rSize.Next(sValues.Length)) };
+                _animals.Add(animal);
+            }
+
+            return _animals;
+        }
+
+        public List<Animal> GenerateSpecificAnimals(Size size, Diet diet, int amount)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            for (int i = 0; i < amount; i++)
+            {
+                Animal animal = new Animal() { Size = size, Diet = diet};
+                animals.Add(animal);
+            }
+
+            return animals;
+        }
+    }
+}
--- CircustreinGUI/CircustreinGUI/Program.cs
+++ CircustreinGUI/CircustreinGUI/Program.cs
@@ -7,93 +7,20 @@ using CircusTreinViewModels;
 
 namespace CircustreinGUI
 {
-    class Program
+    public class Program
     {
         private static AlgorithmViewModel _algo = new AlgorithmViewModel();
         private static List<Animal> _animals = new List<Animal>();
         private static bool _started = true;
         static void Main(string[] args)
         {
-            Random rDiet = new Random();
-            Random rSize = new Random();
-            Array dValues = Enum.GetValues(typeof(Diet));
-            Array sValues = Enum.GetValues(typeof(Size));
-
-            for (int i = 0; i < 16; i++)
-            {
-                Animal animal = new Animal() { Diet = (Diet)dValues.GetValue(rDiet.Next(dValues.Length)), Size = (Size)sValues.GetValue(rSize.Next(sValues.Length))};
-                _animals.Add(animal);
-            }
-
+            _algo = new AlgorithmViewModel();
+            _algo.GenerateRandomAnimals(35);
             var train = Submit(_animals);
             Result(train);
         }
 
-        public static void Menu()
-        {
-            while (_started)
-            {
-                string size = "";
-                Console.WriteLine("Please choose an animal size:\n1. Small\n2. Medium\n3. Large\n");
-                string sizeInput = Console.ReadLine();
-
-                if (sizeInput == "1")
-                {
-                    size = "Small";
-                }
-                else if (sizeInput == "2")
-                {
-                    size = "Medium";
-                }
-                else if (sizeInput == "3")
-                {
-                    size = "Large";
-                }
-                else
-                {
-                    Console.WriteLine("Please choose 1, 2 or 3.\n");
-                }
-
-                string diet = "";
-                Console.WriteLine("Please choose an animal diet\n1. Carnivore\n2. Herbivore");
-                string dietInput = Console.ReadLine();
-
-                if (dietInput == "1")
-                {
-                    diet = "Carnivore";
-                }
-                else if (dietInput == "2")
-                {
-                    diet = "Herbivore";
-                }
-
-                string isFinished;
-                Console.WriteLine("Are you finished? Y/N");
-                isFinished = Console.ReadLine();
-
-                if (isFinished == "Y" || isFinished == "y")
-                {
-                    _started = false;
-                    CreateAnimal(size, diet);
-                    var train = Submit(_animals);
-                    Result(train);
-
-                }
-                else
-                {
-                    CreateAnimal(size, diet);
-                    _started = true;
-                }
-            }
-
-
-        }
-
-        public static void CreateAnimal(string size, string diet)
-        {
-            Animal animal = new Animal() { Size = Enum.Parse<Size>(size), Diet = Enum.Parse<Diet>(diet) };
-            _animals.Add(animal);
-        }
+        
 
         public static Train Submit(List<Animal> animals)
         {
